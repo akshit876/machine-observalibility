1d69dead52065943d4d969f368f0901295f8832e
akhit <s.akhit1267@gmail.com>
2024-08-31 18:25:14 +0530

    reading ascii converted data

--- services/modbus.js
+++ services/modbus.js
@@ -13,6 +13,32 @@ async function connect() {
   }
 }
 
+// Function to convert a number to ASCII characters
+function convertToASCII(number) {
+  let asciiString = "";
+  while (number > 0) {
+    const asciiCode = number & 0xff; // Get the last byte
+    asciiString = String.fromCharCode(asciiCode) + asciiString; // Convert to character and add to the beginning of the string
+    number = number >> 8; // Shift to the next byte
+  }
+  return asciiString;
+}
+
+async function readRegister(address) {
+  try {
+    const { data } = await client.readHoldingRegisters(address, 1);
+    const value = data[0];
+    const asciiString = convertToASCII(value);
+    console.log(
+      `Read register at address ${address}: ${value} (ASCII: ${asciiString})`
+    );
+    return value;
+  } catch (error) {
+    console.error(`Error reading register at address ${address}:`, error);
+    throw error;
+  }
+}
+
 async function monitorRegisters(startAddress, quantity) {
   try {
     let previousValues = [];
@@ -43,15 +69,15 @@ async function monitorRegisters(startAddress, quantity) {
   }
 }
 
-async function readRegister(address) {
-  try {
-    const { data } = await client.readHoldingRegisters(address, 1);
-    return data[0];
-  } catch (error) {
-    console.error(`Error reading register at address ${address}:`, error);
-    throw error;
-  }
-}
+// async function readRegister(address) {
+//   try {
+//     const { data } = await client.readHoldingRegisters(address, 1);
+//     return data[0];
+//   } catch (error) {
+//     console.error(`Error reading register at address ${address}:`, error);
+//     throw error;
+//   }
+// }
 
 async function writeRegister(address, value) {
   try {
